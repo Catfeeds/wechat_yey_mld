3af4f3201a2b4d35deeb8031ec305a5c45458cf5
Company PC <1606822214@qq.com>
2017-04-27 15:56:37 +0800

    更新图标

--- sub/wechat/include/ajax_operate.class.php
+++ sub/wechat/include/ajax_operate.class.php
@@ -43,6 +43,43 @@ class Operate extends Bn_Basic {
 			$this->setReturn ( 'parent.Dialog_Message("对不起！幼儿基本信息的 [证件号码] 已经被注册，请更换！",function(){parent.document.getElementById("Vcl_ID").focus();});' );
 		}
 	}
+	public function Signin($n_uid)
+	{
+		if ($n_uid>0)
+		{
+			
+		}else{
+			$this->setReturn ( 'parent.Common_CloseDialog();parent.Dialog_Error(\'对不起，操作错误，请稍后再试！\');' );
+		}
+		sleep(2);
+		$o_setup=new Admission_Setup(1);
+		$o_date = new DateTime('Asia/Chongqing');
+		$s_date=$o_date->format('Y') . '-' . $o_date->format('m') . '-' . $o_date->format('d');
+		if (strtotime($s_date)<strtotime($o_setup->getSigninStart()))
+		{
+			$this->ReturnMsg('报名开始时间为：'.$o_setup->getSigninStart().' ，请在有效日期内进行报名，谢谢合作。','Name');
+		}
+		$o_stu=new Base_Student_Info();
+		$this->CheckId($n_uid);//验证ID是否有重复
+		if ($this->getPost ( 'Name' )=='')$this->ReturnMsg('基本信息的 [幼儿姓名] 不能为空！','Name');
+		$o_stu->setName($this->getPost ( 'Name' ));
+	    $o_stu->setSex($this->getPost ( 'Sex' ));
+	    $o_stu->setIdType($this->getPost ( 'IdType' ));
+		//验证幼儿ID是否合法
+		if($this->getPost ( 'IdType' )=='居民身份证')
+		{
+			if ($this->validation_filter_id_card($this->getPost ( 'ID' ))==false)$this->ReturnMsg('基本信息的 [幼儿证件号 ] 输入错误！','ID');//验证幼儿ID是否合法
+		}
+	    $o_stu->setId($this->getPost ( 'ID' ));
+	    if ($this->getPost ( 'Birthday' )=='')$this->ReturnMsg('基本信息的 [出生日期] 不能为空！','Birthday');
+	    $o_stu->setBirthday($this->getPost ( 'Birthday' ));
+	    $o_stu=$this->setStuInfo($o_stu);
+	    $o_stu->setSchoolId($o_setup->getDeptId());
+	    $o_stu->setSigninDraft($this->getPost ( 'Draft' ));
+	    $o_stu->Save();
+	   // sleep(2);
+		$this->setReturn ( 'parent.location="'.$this->getPost ( 'Url' ).'register.php?id='.$o_stu->getId().'"' );
+	}
 }
 
 ?>
--- sub/wechat/include/bn_submit.switch.php
+++ sub/wechat/include/bn_submit.switch.php
@@ -6,6 +6,8 @@ header ( 'Pragma: no-cache' );
 header ( 'Expires: Thu, 01 Jan 1970 00:00:00 GMT' );
 header ( 'Last-Modified:' . gmdate ( 'D, d M Y H:i:s' ) . ' GMT' );
 header ( 'content-type:text/html; charset=utf-8' );
+require_once 'bn_session.class.php';
+$O_Session = new Session ();
 require_once 'ajax_operate.class.php';
 $o_operate = new Operate ();
 $s_command = str_replace ( ';', '', $_POST['Vcl_FunName'] );
@@ -14,7 +16,7 @@ if ($s_command=='')
 	$s_command = str_replace ( ';', '', $_GET['Ajax_FunName'] );
 }
 $s_command = str_replace ( ';', '', $s_command );
-$s_command = '$o_operate->' . $s_command . '();';
+$s_command = '$o_operate->' . $s_command . '($O_Session->getUid ());';
 $s_command = str_replace ( '\"', '"', $s_command );
 try {
 	eval ( $s_command );
