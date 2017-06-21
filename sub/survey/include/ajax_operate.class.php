c537bafdb7dadb0fab4c0f6b1ceaa992e72b5473
Company PC <1606822214@qq.com>
2017-06-21 15:22:58 +0800

    初步建立调查问卷模块数据结构

---
+++ sub/survey/include/ajax_operate.class.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting ( 0 );
+require_once RELATIVITY_PATH . 'include/db_table.class.php';
+require_once RELATIVITY_PATH . 'include/db_view.class.php';
+require_once RELATIVITY_PATH . 'include/bn_basic.class.php';
+require_once RELATIVITY_PATH . 'include/bn_user.class.php';
+require_once RELATIVITY_PATH . 'sub/survey/include/db_table.class.php';
+class Operate extends Bn_Basic {
+	protected $N_PageSize= 50;	
+	
+	
+	
+	}
+?>
