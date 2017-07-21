67758943a9a4e94e72c8188c39f2a647c631e045
Company PC <1606822214@qq.com>
2017-07-21 10:38:26 +0800

    新增：教育教学-教师端预览功能

---
+++ sub/wechat/teacher_operation/wei_teach_review.php
@@ -0,0 +1,48 @@
+<?php
+$RELATIVITY_PATH='../../../';
+require_once '../include/it_include.inc.php';
+require_once RELATIVITY_PATH . 'sub/teaching/include/db_table.class.php';
+$s_title='为教学预览';
+//想判断教师权限，是否为绑定用户
+$o_temp=new Base_User_Wechat();
+$o_temp->PushWhere ( array ('&&', 'WechatId', '=',$o_wx_user->getId()) ); 
+if ($o_temp->getAllCount()==0)
+{
+	echo "<script>location.href='access_failed.php'</script>"; 
+	exit(0);
+}
+$o_table=new Teaching_Wei_Teach($_GET['id']);
+require_once '../header.php';
+?>
+<style>
+.weui-media-box_text p{
+	margin-top:10px !important;
+	padding-top:0px !important;
+	padding-bottom:0px !important;
+	font-size:18px !important;
+	line-height:150% !important;;
+}
+.weui-media-box_text b{
+	font-size:20px !important;
+}
+.weui-media-box_text strong{
+	font-size:20px !important;
+}
+</style>
+ 	<div class="page__hd" style="padding:15px;padding-top:0px;">
+        <h1 class="page__title" style="font-size:25px;text-align:center;padding-top:20px;padding-bottom:10px;"><?php echo($o_table->getTitle())?></h1>
+        <div class="weui-media-box_text">
+			<?php echo(rawurldecode($o_table->getComment()))?>
+      	</div>
+      	<iframe style="margin-top:15px;" frameborder="0" width="100%" src="<?php echo($o_table->getVideo())?>" allowfullscreen></iframe>
+    </div>
+
+	<div style="padding:15px;">
+		<a id="next" class="weui-btn weui-btn_default" onclick="document.addEventListener('WeixinJSBridgeReady', WeixinJSBridge.call('closeWindow'));">关闭</a>
+	</div>
+<script type="text/javascript">
+$('iframe').height(Math.floor($('iframe').width()*9/16));
+</script>
+<?php
+require_once '../footer.php';
+?>
